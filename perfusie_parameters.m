3941c747690a1feddfaf92c7cf97da7a08d0054b
bjkeulen <39900544+bjkeulen@users.noreply.github.com>
2018-06-05 09:03:48 +0000

    Update perfusie_parameters.m

--- perfusie_parameters.m
+++ perfusie_parameters.m
@@ -3,15 +3,29 @@
 % 05-06-2018
 
 %% Import data
+all_pt = xlsread();
+[data] = importdata(); 
+t_data = data(:,1);
+y_data = data(:,2);
 
 %% AUC
+auc = trapz(t_data,y_data(:));
+%nog specificeren tot wanneer bij y()
 
 %% AT
+d_data = diff(data);
+i_d_data = find(d_data>1); %cut-off speficiceren
+at = i_d_data(1);
 
 %% MTT
 
+
 %% PD
+pd = max(y_data);
 
 %% TTP
+i_pd = find(y_data==pd);
+ttp = t_data(i_pd);
 
 %% Export data
+fprintf
