8d68ebf5cc0510954dde24f3f1613718d356a8c0
bjkeulen <39900544+bjkeulen@users.noreply.github.com>
2018-06-05 12:59:46 +0000

    Update perfusie_parameters.m

--- perfusie_parameters.m
+++ perfusie_parameters.m
@@ -1,5 +1,5 @@
 % KTO 2018 - GR17019
-% AS17006-TICIScores-EMS
+% AS17006-TICIScores-EMC
 % 05-06-2018
 
 %% Import data
@@ -10,33 +10,24 @@ for i=1:1:length(data.Patientnummer)
   % Import TDC
   addpath(data(i).PadTDC);
   tdc = importdata(data(i).TDC);
-  t_tdc = tdc(:,1);
-  y_tdc = tdc(:,2);
   
   % Normalisatie TDC
 
   % AUC
-  auc = trapz(t_tdc,y_tdc(:));
+  auc = f_auc(tdc);
   data(i).AUC = auc; 
-  %nog specificeren tot wanneer bij y()
 
   % AT
-  d_tdc = diff(tdc);
-  i_d_tdc = find(d_tdc>1); %cut-off speficiceren
-  at = i_d_tdc(1);
+  at = f_at(tdc);
   data(i).AT = at;
 
   % MTT
-  mtt = ;
+  mtt = f_mtt(tdc);
   data(i).MTT = mtt;
 
-  % PD
-  pd = max(y_tdc);
+  % PD en TTP
+  [pd, ttp] = f_pd_ttp(tdc);
   data(i).PD = pd;
-
-  % TTP
-  i_pd = find(y_tdc==pd);
-  ttp = t_tdc(i_pd);
   data(i).TTP = ttp;
 end
 
