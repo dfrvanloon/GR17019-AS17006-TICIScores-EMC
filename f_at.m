063ebb3c3fcfed88ba9c7d19737d445726575481
bjkeulen <39900544+bjkeulen@users.noreply.github.com>
2018-06-06 08:35:49 +0000

    Update f_at.m

--- f_at.m
+++ f_at.m
@@ -1,5 +1,5 @@
-function [auc] = f_at(tdc)
-  d_tdc = diff(tdc);
-  i_d_tdc = find(d_tdc>0); %cut-off specificeren
-  at = i_d_tdc(1);
+function [auc] = f_at(Ca)
+  dCa = diff(Ca);
+  i_dCa = find(dCa>0); %cut-off specificeren
+  at = i_dCa(1);
 end
