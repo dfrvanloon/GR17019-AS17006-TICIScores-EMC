c20501c575cb32c574a8846c3c014f68abce281d
bjkeulen <39900544+bjkeulen@users.noreply.github.com>
2018-06-06 08:26:36 +0000

    Update f_pd_ttp.m

--- f_pd_ttp.m
+++ f_pd_ttp.m
@@ -1,7 +1,5 @@
-function [pd, ttp] = f_pd_ttp(tdc)
-  t_tdc = tdc(:,1);
-  y_tdc = tdc(:,2);
-  pd = max(y_tdc);
-  i_pd = find(y_tdc==pd);
-  ttp = t_tdc(i_pd);
+function [pd, ttp] = f_pd_ttp(t_cbv,y_cbv)
+  pd = max(y_cbv);
+  i_pd = find(y_cbv==pd);
+  ttp = t_cbv(i_pd);
 end
