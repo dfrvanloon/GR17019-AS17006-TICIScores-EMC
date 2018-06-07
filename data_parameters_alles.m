e2383b4d21e11524fb49278289b0cc1639a894fd
bjkeulen <39900544+bjkeulen@users.noreply.github.com>
2018-06-07 14:42:29 +0200

    Create data_parameters_alles.m
    Script om van alle patiënten in 1 keer de parameters te bepalen en alle data in te schrijven. Inclusief toelichting.

---
+++ data_parameters_alles.m
@@ -0,0 +1,75 @@
+% KTO 2018 - GR17019
+% AS17006-TICIScores-EMC
+% 05-06-2018
+
+clear all; close all; clc;
+
+%% Import data
+data = table2struct(readtable('Data.xlsx'));                    % Inleze van de datafile
+
+%% Bepalen parameters
+[N,~] = size(data);                                             % Bepalen van de hoeveelheid patiënten
+for i=1:1:N
+  
+  %% Import en bewerking TIC  
+  % Import
+  addpath(data(i).PadTDC);                                      % Locatie van de tics file toevoegen aan het pad
+  tdc = importdata('test_TICs_R0497_12.tics');                  % Importeren van de tics file van de patiënt, de naam hiervan staat in de datafile
+  t_tdc = tdc(:,1);                                             % Tijdswaarden van de TIC
+  raw_tissue = tdc(:,2);                                        % waarden van het weefsel 
+  raw_aif = tdc(:,3);                                           % waarden van de arteriële input functie (AIF)
+
+  % Fitting
+  f_tissue_fit = fit(t_tdc,raw_tissue,'smoothingspline');       % Fit op TIC met Smoothing Spline fitting, uitkomst is een functie
+  f_aif_fit = fit(t_tdc,raw_aif,'smoothingspline');             % Hetzelfde voor de AIF
+  
+  t = 0:0.1:max(t_tdc);                                         % Zelf bepaalde, vaste tijdsvector
+  y_tissue_fit = feval(f_tissue_fit,t);                         % Tijdsvector wordt gebruikt om de Y-waarden van de gefitte functie te verkrijgen
+  y_aif_fit = feval(f_aif_fit,t);                               % Hetzelfde voor de AIF
+
+  % Normalisatie naar baseline
+  y_tissue = bf(y_tissue_fit,[0 length(t)],'pchip');            % Grafieken worden naar baseline gebracht m.b.v. een script die beschikbaar was via de File Exchange
+  y_tissue(y_tissue<0) = 0;                                     % van MATLAB. Op basis van de eerste en de laatste waarde van de grafiek wordt er een functie bepaald
+  y_aif = bf(y_aif_fit,[0 length(t)],'pchip');                  % Die deze punten en daarmee de hele grafiek weer naar 0 brengt.
+  y_aif(y_aif<0) = 0;                                           % Negatieve waarden a.g.v. de normalisatie worden op 0 gezet.
+
+  %% Berekenen Cu, Ca en R
+  Ftissue = fft(y_tissue);                                      % Fourier transformatie van de gefitte, genormaliseerde grafiek
+  Faif = fft(y_aif);                                
+  ht = ifft(Ftissue./Faif);                                     % Deconvolutie om de transferfunctie van Y_tissue(t) = AIF(t) (conv) H(t) te bepalen. Incl. inverse Fourier
+  R = 1 - trapz(t,ht);                                          % Bepaling van de residufunctie R(t)
+
+  %% AUC
+  auc = 1;
+  data(i).AUC = auc;                                            % Inschrijven van de AUC in de datafile bij de patiënt
+
+  %% AT
+  d_tissue = diff(y_tissue)/t(1:length(t)-1)';                  % Afgeleide van de weefsel-TIC
+  i_dtissue = find(d_tissue>0);                                 % Indices van de hellingen groter dan de cut-off
+  at = t(i_dtissue(1));                                         % Eerste tijdstip waarop de helling hoger is dan de cut-off, het moment waarop de bolus in het weefsel is aangekomen (AT)                             
+  data(i).AT = at;                                              % Inschrijven van de arrival time in de datafile bij de patiënt 
+
+  %% MTT
+  cbv = trapz(t,y_tissue);                                      % AUC van de weefsel-TIC. Dit is het cerebral blood volume (CBV)
+  cbf = max(R);                                                 % Hoogste punt van de residufunctie. Dit is de cerebral blood flow (CBF)
+  mtt = cbv/cbf;                                                % De mean transit time is het CBV/CBF
+  data(i).MTT = mtt;                                            % Inschrijven van de MTT in de datafile bij de patiënt
+
+  %% PD
+  pd = max(y_tissue);                                           % De peak density (PD) is de maximale dichtheid in het weefsel, dus de piek van de weefsel-TIC
+  data(i).PD = pd;                                              % Inschrijven van de PD in de datafile bij de patiënt
+  
+  %% TTP
+  i_pd = find(y_tissue==pd);                                    % De index van de PD waarde
+  ttp = t(i_pd);                                                % De time to peak (TTP) is de t behorende bij de PD
+  data(i).TTP = ttp;                                            % Inschrijven van de TTP in de datafile bij de patiënt
+  
+end
+
+%% Export data
+data_cell = struct2cell(data);                                  % De datafile omschrijven van struct naar cell formaat
+xlswrite('data_parameters.xlsx',data_cell')                     % Schrijven van een Excelbestand met de datafile als cell
+%open('data_parameters.xlsx')                                    
+
+% data_table = struct2table(data);                              
+% writetable(data_table,'data_parameters')
