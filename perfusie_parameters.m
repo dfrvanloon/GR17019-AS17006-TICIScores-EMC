30f27ab307d9f331a96a456c58f199c955a5bf40
bjkeulen <39900544+bjkeulen@users.noreply.github.com>
2018-06-07 12:33:26 +0200

    Create perfusie_parameters.m
    Verbeterde versie met fitting en normalisatie. Geeft excel-bestand met de 5 parameters als output.

---
+++ perfusie_parameters.m
@@ -0,0 +1,89 @@
+% KTO 2018 - GR17019
+% AS17006-TICIScores-EMC
+% 05-06-2018
+
+clear all; close all; clc;
+
+%% Import data
+%data = table2struct(readtable(<%PATIENTDATA>.txt));
+
+%% Bepalen parameters
+%for i=1:1:length(data.Patientnummer)
+
+  %% Import en bewerking TDC
+  %addpath(data(i).PadTDC);
+  
+  % Import
+  tdc = importdata('test_TICs_R0497_12.tics');
+  t_tdc = tdc(:,1);
+  raw_tissue = tdc(:,2);
+  raw_aif = tdc(:,3);
+
+  % Fitting
+  f_tissue_fit = fit(t_tdc,raw_tissue,'smoothingspline');
+  f_aif_fit = fit(t_tdc,raw_aif,'smoothingspline');
+  
+  t = 0:0.1:max(t_tdc);
+  y_tissue_fit = feval(f_tissue_fit,t);
+  y_aif_fit = feval(f_aif_fit,t);
+
+  % Normalisatie naar baseline
+  y_tissue = bf(y_tissue_fit,[0 length(t)],'pchip');
+  y_tissue(y_tissue<0) = 0;
+  y_aif = bf(y_aif_fit,[0 length(t)],'pchip');
+  y_aif(y_aif<0) = 0;
+  
+  %% Plotten grafieken
+  figure; hold on;
+  plot(t_tdc,raw_tissue,'r','LineWidth',1)
+  plot(t,y_tissue_fit,'b','LineWidth',1)
+  plot(t,y_tissue,'g','LineWidth',1)
+  title('TDC en bewerkte TDC van het weefsel')
+  legend('Ruwe TDC data','Functie gefit op data','Functie genormaliseerd naar baseline')
+  xlabel('Tijd [s]'); ylabel('Grijswaarde []')
+  
+  figure; hold on;
+  plot(t_tdc,raw_aif,'r','LineWidth',1)
+  plot(t,y_aif_fit,'b','LineWidth',1)
+  plot(t,y_aif,'g','LineWidth',1)
+  title('TDC en bewerkte TDC van de arteriële input')
+  legend('Ruwe TDC data','Functie gefit op data','Functie genormaliseerd naar baseline')
+  xlabel('Tijd [s]'); ylabel('Grijswaarde []')
+
+  %% Berekenen Cu, Ca en R
+  Ftissue = fft(y_tissue);
+  Faif = fft(y_aif);
+  ht = ifft(Ftissue./Faif);
+  R = 1 - trapz(t,ht);
+
+  %% AUC
+  auc = 1;
+  data.AUC = auc; 
+
+  %% AT
+  d_tissue = diff(y_tissue)/t(1:length(t)-1)';
+  i_dtissue = find(d_tissue>0); %cut-off specificeren
+  at = i_dtissue(1);
+  data.AT = at;
+
+  %% MTT
+  cbv = trapz(t,y_tissue);
+  cbf = max(R);
+  mtt = cbv/cbf;
+  data.MTT = mtt;
+
+  %% PD
+  pd = max(y_tissue);
+  i_pd = find(y_tissue==pd);
+  data.PD = pd;
+  
+  %% TTP
+  ttp = t(i_pd);
+  data.TTP = ttp;
+  
+%end
+
+%% Export data
+data_cell = struct2cell(data);
+xlswrite('data_parameters.xlsx',data_cell)
+open('data_parameters.xlsx')
